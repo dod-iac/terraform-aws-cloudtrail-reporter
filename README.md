9c8dfa42f1e7c06abe6abba5efd0f164f730ad3a
Robert <robert.sivilli@dds.mil>
2022-03-23 13:05:49 -0600

    init commit

--- README.md
+++ README.md
@@ -1,96 +1,53 @@
 <!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
-# Terraform Module template
-
-This repository is meant to be a template for creating new terraform modules.
-
-## Creating a new Terraform Module
-
-1. Clone this repo, renaming appropriately.
-1. Write your terraform code in the root dir.
-1. Ensure you've completed the [Developer Setup](#developer-setup).
-1. In the root dir, modify the `module` line for the repo path. Then run `make tidy`, which updates the `go.sum` file and downloads dependencies.
-1. Update the terratest tests in the examples and test directories.
-1. Run your terratest tests to ensure they work as expected using instructions below.
-
----
-
-<!-- DELETE ABOVE THIS LINE -->
+# Cloudtrail Reporter
 
 ## Description
 
-Please put a description of what this module does here
+This module configures cloudtrail and reporting to a central monitoring hub
 
 ## Usage
 
-Add Usage information here
+Use in combination with a report receiver deployed in central monitoring environment
 
 Resources:
 
-* [Article Example](https://article.example.com)
-
-```hcl
-module "example" {
-  source = "dod-iac/example/aws"
-
-  tags = {
-    Project     = var.project
-    Application = var.application
-    Environment = var.environment
-    Automation  = "Terraform"
-  }
-}
-```
-
-## Testing
-
-Run all terratest tests using the `terratest` script.  If using `aws-vault`, you could use `aws-vault exec $AWS_PROFILE -- terratest`.  The `AWS_DEFAULT_REGION` environment variable is required by the tests.  Use `TT_SKIP_DESTROY=1` to not destroy the infrastructure created during the tests.  Use `TT_VERBOSE=1` to log all tests as they are run.  Use `TT_TIMEOUT` to set the timeout for the tests, with the value being in the Go format, e.g., 15m.  The go test command can be executed directly, too.
-
-## Terraform Version
-
-Terraform 0.13. Pin module version to ~> 1.0.0 . Submit pull-requests to master branch.
-
-Terraform 0.11 and 0.12 are not supported.
-
-## License
-
-This project constitutes a work of the United States Government and is not subject to domestic copyright protection under 17 USC § 105.  However, because the project utilizes code licensed from contributors and other third parties, it therefore is licensed under the MIT License.  See LICENSE file for more information.
-
-## Developer Setup
-
-This template is configured to use aws-vault, direnv, go, pre-commit, terraform-docs, and tfenv.  If using Homebrew on macOS, you can install the dependencies using the following code.
-
-```shell
-brew install aws-vault direnv go pre-commit terraform-docs tfenv
-pre-commit install --install-hooks
-```
-
-If using `direnv`, add a `.envrc.local` that sets the default AWS region, e.g., `export AWS_DEFAULT_REGION=us-west-2`.
-
-If using `tfenv`, then add a `.terraform-version` to the project root dir, with the version you would like to use.
+TODO update example
 
 ## Requirements
 
 | Name | Version |
 |------|---------|
-| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 0.13 |
-| <a name="requirement_aws"></a> [aws](#requirement\_aws) | ~> 3.0 |
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.0 |
+| <a name="requirement_aws"></a> [aws](#requirement\_aws) | ~> 3.55 |
 
 ## Providers
 
 | Name | Version |
 |------|---------|
-| <a name="provider_aws"></a> [aws](#provider\_aws) | ~> 3.0 |
+| <a name="provider_aws"></a> [aws](#provider\_aws) | ~> 3.55 |
+| <a name="provider_random"></a> [random](#provider\_random) | n/a |
 
 ## Modules
 
-No modules.
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_cloudwatch_kms_key"></a> [cloudwatch\_kms\_key](#module\_cloudwatch\_kms\_key) | dod-iac/cloudwatch-kms-key/aws | >1.0.0 |
+| <a name="module_logging_bucket"></a> [logging\_bucket](#module\_logging\_bucket) | trussworks/s3-private-bucket/aws | >=3.7.1 |
+| <a name="module_s3_kms_key"></a> [s3\_kms\_key](#module\_s3\_kms\_key) | git::https://github.com/dod-iac/terraform-aws-s3-kms-key.git | rms-extending-principals |
 
 ## Resources
 
 | Name | Type |
 |------|------|
+| [aws_cloudtrail.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudtrail) | resource |
+| [aws_cloudwatch_log_group.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group) | resource |
+| [aws_cloudwatch_log_subscription_filter.test_lambdafunction_logfilter](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_subscription_filter) | resource |
+| [aws_iam_policy.logging](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy) | resource |
+| [aws_iam_role.cloudwatch](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role) | resource |
+| [random_string.prefix](https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string) | resource |
 | [aws_caller_identity.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity) | data source |
 | [aws_iam_account_alias.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_account_alias) | data source |
+| [aws_iam_policy_document.this](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document) | data source |
 | [aws_partition.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/partition) | data source |
 | [aws_region.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/region) | data source |
 
@@ -98,7 +55,10 @@ No modules.
 
 | Name | Description | Type | Default | Required |
 |------|-------------|------|---------|:--------:|
-| <a name="input_tags"></a> [tags](#input\_tags) | Tags applied to the AWS resources. | `map(string)` | `{}` | no |
+| <a name="input_include_global_service_events"></a> [include\_global\_service\_events](#input\_include\_global\_service\_events) | n/a | `bool` | `true` | no |
+| <a name="input_kinesis_stream_arn"></a> [kinesis\_stream\_arn](#input\_kinesis\_stream\_arn) | n/a | `string` | n/a | yes |
+| <a name="input_role_arn"></a> [role\_arn](#input\_role\_arn) | n/a | `string` | n/a | yes |
+| <a name="input_tags"></a> [tags](#input\_tags) | n/a | <pre>object({<br>    Project     = string<br>    Environment = string<br>    Application = string<br>  })</pre> | <pre>{<br>  "Application": "infra",<br>  "Environment": "dev",<br>  "Project": "elmo"<br>}</pre> | no |
 
 ## Outputs
 
--- main.tf
+++ main.tf
@@ -1,45 +1,33 @@
 /**
- * # Terraform Module template
- *
- * This repository is meant to be a template for creating new terraform modules.
- *
- * ## Creating a new Terraform Module
- *
- * 1. Clone this repo, renaming appropriately.
- * 1. Write your terraform code in the root dir.
- * 1. Ensure you've completed the [Developer Setup](#developer-setup).
- * 1. In the root dir, modify the `module` line for the repo path. Then run `make tidy`, which updates the `go.sum` file and downloads dependencies.
- * 1. Update the terratest tests in the examples and test directories.
- * 1. Run your terratest tests to ensure they work as expected using instructions below.
- *
- * ---
- *
- * <!-- DELETE ABOVE THIS LINE -->
+ * # Cloudtrail Reporter
  *
  * ## Description
  *
- * Please put a description of what this module does here
+ * This module configures cloudtrail and reporting to a central monitoring hub
  *
  * ## Usage
  *
- * Add Usage information here
+ * Use in combination with a report receiver deployed in central monitoring environment
  *
  * Resources:
  *
- * * [Article Example](https://article.example.com)
- *
- * ```hcl
- * module "example" {
- *   source = "dod-iac/example/aws"
- *
- *   tags = {
- *     Project     = var.project
- *     Application = var.application
- *     Environment = var.environment
- *     Automation  = "Terraform"
- *   }
- * }
- * ```
+ * TODO update example
+
+ ```hcl
+
+  module "example" {
+    source = "dod-iac/example/aws"
+
+    tags = {
+      Project     = var.project
+      Application = var.application
+      Environment = var.environment
+      Automation  = "Terraform"
+    }
+  }
+
+  ```
+
  *
  * ## Testing
  *
@@ -70,8 +58,178 @@
  *
  *
  */
+// =================================================================
+//
+// Work of the U.S. Department of Defense, Defense Digital Service.
+// Released as open source under the MIT License.  See LICENSE file.
+//
+// =================================================================
 
 data "aws_caller_identity" "current" {}
-data "aws_iam_account_alias" "current" {}
 data "aws_partition" "current" {}
 data "aws_region" "current" {}
+data "aws_iam_account_alias" "current" {}
+
+
+resource "aws_cloudtrail" "this" {
+  is_multi_region_trail         = true
+  name                          = format("%s-cloudtrail", local.name_prefix)
+  s3_bucket_name                = module.logging_bucket.id
+  s3_key_prefix                 = format("%s-cloudtrail", local.name_prefix)
+  include_global_service_events = var.include_global_service_events
+  kms_key_id                    = module.s3_kms_key.aws_kms_key_arn
+  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.this.arn}:*"
+  cloud_watch_logs_role_arn     = aws_iam_role.cloudwatch.arn
+  enable_log_file_validation    = true
+}
+locals {
+  bucket_prefix = format("%s-", data.aws_iam_account_alias.current.account_alias)
+  bucket_id     = "${local.bucket_prefix}${format("%s-logging", local.name_prefix)}"
+}
+resource "aws_cloudwatch_log_group" "this" {
+  name_prefix       = format("%s-cloudtrail-cloudwatch", local.name_prefix)
+  tags              = var.tags
+  kms_key_id        = module.cloudwatch_kms_key.aws_kms_key_arn
+  retention_in_days = 1
+}
+
+
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    sid       = "AWSCloudTrailAclCheck20211208"
+    actions   = ["s3:GetBucketAcl"]
+    effect    = "Allow"
+    resources = ["arn:${data.aws_partition.current.partition}:s3:::${local.bucket_id}"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+  }
+  statement {
+    sid       = "AWSCloudTrailWrite20211208"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:${data.aws_partition.current.partition}:s3:::${local.bucket_id}/*"]
+    effect    = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+  }
+}
+
+module "logging_bucket" {
+  source               = "trussworks/s3-private-bucket/aws"
+  bucket               = format("%s-logging", local.name_prefix)
+  version              = ">=3.7.1"
+  kms_master_key_id    = module.s3_kms_key.aws_kms_key_id
+  custom_bucket_policy = data.aws_iam_policy_document.this.json
+  sse_algorithm        = "aws:kms"
+
+  tags = merge(var.tags, {
+    Name = format("%s-cloudtrail-bucket", local.name_prefix)
+    }
+  )
+  transitions = [
+    {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    },
+    {
+      days          = 60
+      storage_class = "GLACIER"
+    },
+    {
+      days          = 150
+      storage_class = "DEEP_ARCHIVE"
+    }
+  ]
+
+  noncurrent_version_transitions = [
+    {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    },
+    {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+  ]
+}
+
+
+module "s3_kms_key" {
+  #source = "dod-iac/s3-kms-key/aws"
+  source      = "git::https://github.com/dod-iac/terraform-aws-s3-kms-key.git?ref=rms-extending-principals"
+  name        = format("alias/%s-logging-kms", local.name_prefix)
+  description = format("A KMS key used to encrypt objects at rest in S3 for %s:%s.", local.application, local.environment)
+  principals = [
+    { identifiers = ["cloudtrail.amazonaws.com"], type = "Service" }
+  ]
+  tags = var.tags
+}
+
+module "cloudwatch_kms_key" {
+  source  = "dod-iac/cloudwatch-kms-key/aws"
+  name    = "alias/name"
+  tags    = var.tags
+  version = ">1.0.0"
+}
+
+
+
+resource "aws_iam_policy" "logging" {
+  name        = format("%s-cloudtrail-logging", local.name_prefix)
+  path        = "/"
+  description = "IAM policy for logging from lambda"
+
+  policy = jsonencode(
+    {
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          Resource = [aws_cloudwatch_log_group.this.arn, "${aws_cloudwatch_log_group.this.arn}:*"]
+          Effect   = "Allow"
+        },
+        {
+          Action   = ["kms:Encrypt", "kms:Decrypt"]
+          Resource = [module.cloudwatch_kms_key.aws_kms_key_arn]
+          Effect   = "Allow"
+
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role" "cloudwatch" {
+  name                = format("%s-cloudtrail-cloudwatch-role", local.name_prefix)
+  managed_policy_arns = [aws_iam_policy.logging.arn]
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "test_lambdafunction_logfilter" {
+  name            = format("%s-cloudtrail-subscription", local.name_prefix)
+  role_arn        = var.role_arn
+  log_group_name  = aws_cloudwatch_log_group.this.name
+  filter_pattern  = "{$.readOnly is FALSE}"
+  destination_arn = var.kinesis_stream_arn
+  distribution    = "Random"
+}
