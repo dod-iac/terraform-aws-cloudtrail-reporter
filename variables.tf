28f1c658e0257274cf457930d865cae3ffd842be
rsivilli <rsivilli@users.noreply.github.com>
2022-03-23 13:06:16 -0600

    init commit (#1)

--- variables.tf
+++ variables.tf
@@ -1,7 +1,43 @@
-# Variables
-
 variable "tags" {
-  type        = map(string)
-  description = "Tags applied to the AWS resources."
-  default     = {}
+  type = object({
+    Project     = string
+    Environment = string
+    Application = string
+  })
+  default = {
+    Project     = "elmo"
+    Environment = "dev"
+    Application = "infra"
+  }
+}
+
+variable "include_global_service_events" {
+  type    = bool
+  default = true
+}
+variable "role_arn" {
+  type = string
+}
+variable "kinesis_stream_arn" {
+  type = string
+}
+
+
+locals {
+  project     = var.tags.Project
+  environment = var.tags.Environment
+  application = var.tags.Application
+  name_prefix = format("%s-%s-%s-%s", random_string.prefix.id, local.project, local.application, local.environment)
+}
+
+resource "random_string" "prefix" {
+  keepers = {
+    project     = local.project
+    environment = local.environment
+    application = local.application
+  }
+  length  = 8
+  special = false
+  upper   = false
+
 }
